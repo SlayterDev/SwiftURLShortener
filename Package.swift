9e0e9c7ca1141e80a99200d76ddcb7714a190d80
bslayter <bslayter@axxess.com>
2017-07-12 10:53:59 -0500

    Project rename

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-	name: "MyFirstBackend",
+	name: "SwiftURLShortener",
 	dependencies: [
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2),
 		.Package(url: "https://github.com/SwiftORM/MySQL-StORM.git", majorVersion: 1),
